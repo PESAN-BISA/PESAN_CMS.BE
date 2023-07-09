357a8c64b354cf434ba1178cdb5f9e3ed5e09a34
Fikri Alwan Ramadhan <fikri.alwan@ai.astra.co.id>
2023-07-09 16:12:40 +0700

    feat(auth): return jwt token in login api

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.userService.findOne(username);
-    if (user?.password !== pass) {
+    if (!user || user?.password !== pass) {
       throw new UnauthorizedException();
     }
-
-    const { password, ...result } = user;
-
-    return result;
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
