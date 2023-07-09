66fff373cf1b181e124c464acdd332771cc25316
Fikri Alwan Ramadhan <fikri.alwan@ai.astra.co.id>
2023-07-09 15:25:57 +0700

    chore: add and setup nest config depedency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
